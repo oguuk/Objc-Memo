3ae46acd3fa3760d5f67ff7e604a84ef4ae3bb3b
Gukwon Oh <ogw135@gmail.com>
2023-02-22 11:46:59 +0900

    [feat] : DetailVC 추가 &  메모 보기 ui 설정

---
+++ MemoApp/MemoApp/ViewController/Detail/DetailViewController.h
@@ -0,0 +1,16 @@
+//
+//  DetailViewController.h
+//  MemoApp
+//
+//  Created by 오국원 on 2023/02/22.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface DetailViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
