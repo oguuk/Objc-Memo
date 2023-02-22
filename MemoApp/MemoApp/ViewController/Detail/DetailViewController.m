640ad031eab34b614f611b4495cb08dc5588083b
Gukwon Oh <ogw135@gmail.com>
2023-02-22 12:11:17 +0900

    [feat] : 메모 정보 출력

--- MemoApp/MemoApp/ViewController/Detail/DetailViewController.m
+++ MemoApp/MemoApp/ViewController/Detail/DetailViewController.m
@@ -9,6 +9,8 @@
 
 @interface DetailViewController () <UITableViewDataSource>
 
+@property (strong, nonatomic) NSDateFormatter* formatter;
+
 @end
 
 @implementation DetailViewController
@@ -20,10 +22,11 @@
 - (UITableViewCell*)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
     if (indexPath.row == 0) {
         UITableViewCell* cell = [tableView dequeueReusableCellWithIdentifier:@"memoCell" forIndexPath:indexPath];
+        cell.textLabel.text = self.memo.content;
         return cell;
     } else if (indexPath.row == 1) {
         UITableViewCell* cell = [tableView dequeueReusableCellWithIdentifier:@"dateCell" forIndexPath:indexPath];
-        
+        cell.textLabel.text = [self.formatter stringFromDate:self.memo.insertDate];
         return cell;
     }
     return  [[UITableViewCell alloc] init];
@@ -32,6 +35,10 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    self.formatter = [[NSDateFormatter alloc] init];
+    self.formatter.dateStyle = NSDateFormatterLongStyle;
+    self.formatter.timeStyle = NSDateFormatterMediumStyle;
+    self.formatter.locale = [NSLocale localeWithLocaleIdentifier:@"Ko_kr"];
 }
 
 /*
